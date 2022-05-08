e6f6d2df94a5fba240351fd28c5127d430b21c32
chrisbowser <bowser3790@gmail.com>
2022-05-08 18:50:45 -0400

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = Phrase;
 
 
+
 // Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("")
+// function reverse(string) {
+//   return Array.from(string).reverse().join("")
+// }
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
 }
 
 // defines a Phrase Object
@@ -18,14 +22,14 @@ function Phrase(content) {
 
 // Returns content processed for palindrome testing.
 
-// this.processedContent = function processedContent() {
-//   return this.content.toLowerCase();
-// }
+this.processedContent = function processedContent() {
+  return this.letters().toLowerCase();
+}
 
-// returns content processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.translation.toLowerCase();
-  }
+// returns translation processed for palindrome testing.
+  // this.processedContent = function processedContent() {
+  //   return this.translation.toLowerCase();
+  // }
 
 
 // Returns true for a palindrome, false otherwise
@@ -40,20 +44,58 @@ function Phrase(content) {
 //   return processedContent === reverse(processedContent);
 // }
 
+// returns the letters in the content
+// starting to refactor this loop this is partially refactored but want to keep
+// this below for reference.
+  // this.letters = function letters() {
+  //   let theLetters = [];
+  //   const letterRegEx = /[a-z]/i;  // i is for case insensetive for regex
+  //   for (let i = 0; i < this.content.length; i++) {
+  //     let charecter = this.content.charAt(i);
+  //     if (charecter.match(letterRegEx)) {
+  //       theLetters.push(charecter);
+  //     }
+  //   }
+  //   return theLetters.join("");
+  // }
+
+// Refactored version of the above code
+this.letters = function letters() {
+  return Array.from(this.content).filter(c => c.match(/[a-z]/i)).join("");
+}
+  // this.content = "Madam, I'm Adam"
+
+  // all the code below is broken down into 1 line with the above code
+  // this.letters = function letters() {
+  // let theLetters = [];
+  // const letterRegEx = /[a-z]/i;  // i is for case insensetive for regex
+  // Array.from(this.content).forEach(function(charecter) {
+  //   if (charecter.match(letterRegEx)) {
+  //     theLetters.push(charecter);
+  //   }
+  // });
+  // return theLetters.join("");
+
+// broken down the furthest so far. below is the refactored code above refactor
+// of the refactor.
+this.letters = function letters() {
+  return (this.content.match(/[a-z]/gi) || []).join("");
+}
+
 
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return this.processedContent() === reverse(this.processedContent());
+    //let processedContent = this.content.toLowerCase();
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
 
 // Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-}
-
-TranslatedPhrase.prototype = new Phrase();
+// function TranslatedPhrase(content, translation) {
+//   this.content = content;
+//   this.translation = translation;
+// }
+//
+// TranslatedPhrase.prototype = new Phrase();
 
 
 // Add a blank method to the String object prototype that returns true if
@@ -62,24 +104,24 @@ TranslatedPhrase.prototype = new Phrase();
 // regex syntax for the start and end of a string.
 
 
-String.prototype.blank = function blank() {
-  return !!(this.match(/^\s*$/g));
-}
-console.log("foobar" .blank());
-console.log("    foobar".blank())
-console.log("   foobar   ".blank());
-console.log("".blank());
-
-
-// Using whichever method you prefer (direct indexing or slicing), add a last
-// method to the Array object prototype that returns the last element of an
-// array. Hint: Refer to Section 3.3.
-
-
-a = [56, 60, 100, 110]
-
-Array.prototype.last = function() {
-	return this[this.length-1];
-}
-
-a.last();
+// String.prototype.blank = function blank() {
+//   return !!(this.match(/^\s*$/g));
+// }
+// console.log("foobar" .blank());
+// console.log("    foobar".blank())
+// console.log("   foobar   ".blank());
+// console.log("".blank());
+//
+//
+// // Using whichever method you prefer (direct indexing or slicing), add a last
+// // method to the Array object prototype that returns the last element of an
+// // array. Hint: Refer to Section 3.3.
+//
+//
+// a = [56, 60, 100, 110]
+//
+// Array.prototype.last = function() {
+// 	return this[this.length-1];
+// }
+//
+// a.last();
---
+++ test/test.js
@@ -0,0 +1,46 @@
+let assert = require("assert");
+let Phrase = require("../index.js");
+
+
+// "Phrase" <= This is a sting as an argument and an anonomous function as an
+// arguement * its a function of 0 arguments
+// Here we are going to be describing the palendrome method from index.js
+// there is a standard way of denoting the method is part of an Object
+// which is to say it can be called on instances of that Object like This
+// Phrase#palindrome
+// below shows a nested describe block
+// it is a function that takes in an argument describing what we are testing
+
+describe("Phrase", function(){
+  // Phrase#palindrome
+  describe("#palindrome", function(){
+
+    it("should return false for a non-palindrome", function(){
+      let nonPalindrome = new Phrase("apple");
+      assert(!nonPalindrome.palindrome());
+    });
+
+    it("should return true for a plain palindrome", function(){
+      let plainPalindrome = new Phrase("racecar");
+      assert(plainPalindrome.palindrome());
+    });
+
+    it ("should return true for mixed case palindrome", function() {
+      let mixedCase = new Phrase("RaceCar");
+      assert(mixedCase.palindrome());
+
+      });
+
+    it ("should return true for mixed case palindrome", function() {
+      let punctuationPal = new Phrase("Madam, I'm Adam.");
+      assert(punctuationPal.palindrome());
+    });
+
+    describe("#letters", function(){
+      it ("should return only letters", function() {
+        let punctuatedPal = new Phrase("Madam, I'm Adam");
+        assert.strictEqual(punctuatedPal.letters(), "MadamImAdam");
+      });
+    });
+  });
+});
